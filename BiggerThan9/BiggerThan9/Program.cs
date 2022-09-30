eef31803756004749a2ba619a1f62a87ef446e42
Ivo Petkov <79451935+verstrix@users.noreply.github.com>
2022-09-30 15:44:40 +0300

    Add files via upload

---
+++ BiggerThan9/BiggerThan9/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace BiggerThan9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 1)
+            {
+                Console.WriteLine("One");
+            }
+            if (number == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            if (number == 3)
+            {
+                Console.WriteLine("Three");
+            }
+            if (number == 4)
+            {
+                Console.WriteLine("Four");
+            }
+            if (number == 5)
+            {
+                Console.WriteLine("Five");
+            }
+            if (number == 6)
+            {
+                Console.WriteLine("Six");
+            }
+            if (number == 7)
+            {
+                Console.WriteLine("Seven");
+            }
+            if (number == 8)
+            {
+                Console.WriteLine("Eight");
+            }
+            if (number == 9)
+            {
+                Console.WriteLine("Nine");
+            }
+            if (number > 9)
+            {
+                Console.WriteLine("Too big");
+            }
+
+        }
+    }
+}
---
+++ GreaterNumber/GreaterNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Transactions;
+
+namespace GreaterNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter two numbers");
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            
+            if (numberOne > numberTwo)
+            {
+                Console.WriteLine(numberOne);
+            }
+            else
+            {
+                Console.WriteLine(numberTwo);
+            }
+
+
+
+        }
+    }
+}
