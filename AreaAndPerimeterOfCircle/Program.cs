155e1371b34d92803c66778ecd5f83d751ac6f84
Ivo Petkov <79451935+verstrix@users.noreply.github.com>
2022-09-30 15:47:41 +0300

    Add files via upload

---
+++ AreaAndPerimeterOfCircle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AreaAndPerimeterOfCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double perimeter = 2 * Math.PI * radius;
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine($"The perimeter of the circle is {perimeter:f2}");
+            Console.WriteLine($"The area of the circle is {area:f2}");
+
+
+        }
+    }
+}
---
+++ AreaOfTriangle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AreaOfTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the height of triangle");
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter one side of the triangle");
+            double side = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("The area of triangle is " + side * height / 2);
+        }
+    }
+}
