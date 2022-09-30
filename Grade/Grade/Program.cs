ec34852df995d995f99f878cd9043295af90ff96
Ivo Petkov <79451935+verstrix@users.noreply.github.com>
2022-09-30 15:46:20 +0300

    Add files via upload

---
+++ Grade/Grade/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Grade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            /*
+            Console.WriteLine("Type your grade");
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            */
+
+
+            }
+        }
+    }
+
