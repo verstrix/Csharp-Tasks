32a22f508c27b550e841987c4bb3a80edb46c9f6
Ivo Petkov <79451935+verstrix@users.noreply.github.com>
2022-09-30 18:48:06 +0300

    Add files via upload

---
+++ CelsiusToFahrenheit/CelsiusToFahrenheit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CelsiusToFahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the temperature in celsius to convert to fahrenheit.");
+            double celsius = double.Parse(Console.ReadLine());
+            double fahrenheit = celsius * 9/5 + 32;
+
+            Console.WriteLine($"The temperature in fahrenheit is {fahrenheit:f2}");
+
+
+
+
+        }
+    }
+}
---
+++ TheWeatherOutside/TheWeatherOutside/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TheWeatherOutside
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var weather = Console.ReadLine();
+
+            if (weather == "sunny")
+            {
+                Console.WriteLine("It's warm!");
+            }
+            else
+            {
+                Console.WriteLine("It's cold!");
+            }
+        }
+    }
+}
